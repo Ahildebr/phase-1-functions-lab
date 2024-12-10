cf51e6bdc9dae1e0fd60234e4c1a2361ddd39f3c
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2024-12-10 06:42:26 -0800

    makes changes to Index.js

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+// Block Function
+function distanceFromHqInBlocks(blocks) {
+   const hq = 42
+   let blocksDistance
+
+    if (blocks >= hq) {
+        blocksDistance = blocks - hq
+        return blocksDistance
+    }
+    else {
+        blocksDistance = hq - blocks
+        return blocksDistance
+    }   
+}
+
+//Feet Function
+function distanceFromHqInFeet(blocks) { 
+    const feetPerBlock = 264
+    const hq = 42
+    let blocksDistance
+     
+         if (blocks >= hq) {
+             blocksDistance = blocks - hq
+            
+         }
+         else {
+             blocksDistance = hq - blocks
+             
+         }   
+    return blocksDistance * feetPerBlock
+}
+
+//Distance Travelled in Feet (hopefully wearing shoes)
+function distanceTravelledInFeet(start, end) {
+    const feetPerBlock = 264
+    let distance
+    let feetDistance 
+    if (start >= end) {
+        distance = start - end
+    }
+    else {
+        distance = end - start
+    }
+    feetDistance = distance * feetPerBlock
+    return feetDistance
+}
+
+ 
